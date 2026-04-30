c3fdb4b12c4b7b7f45599fdca15e4ba2c54c2e8c
Aayush Ranjan <aayush.ranjan0904@gmail.com>
2026-05-01 00:37:29 +0530

    add gun and animations

--- ui.gd
+++ ui.gd
@@ -1,22 +1,23 @@
 extends CanvasLayer
 
-var ammo = 0
-var current_weapon = "knife"
 
 func _ready():
 	$AnimatedSprite2D.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
-		if current_weapon == "knife":
+		if Global.current_weapon == "knife":
 			$AnimatedSprite2D.play("stab")
-		elif current_weapon == "gun":
-			if ammo > 0:
+		elif Global.current_weapon == "gun":
+			if Global.ammo > 0:
 				$AnimatedSprite2D.play("shoot")
-				ammo -= 1
+				Global.ammo -= 1
+			else:
+				Global.current_weapon = "knife"
+				$AnimatedSprite2D.play("knife_idle")
 
 func _on_AnimatedSprite2D_animation_finished():
-	if current_weapon == "knife":
+	if Global.current_weapon == "knife":
 		$AnimatedSprite2D.play("knife_idle")
-	elif current_weapon == "gun":
+	elif Global.current_weapon == "gun":
 		$AnimatedSprite2D.play("gun_idle")
